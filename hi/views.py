020515fcc304a84fdbe6f5a1e92642f1278f887c
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-2.compute.internal>
2021-06-27 10:29:34 +0000

    urls.py change

--- hi/views.py
+++ hi/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import datetime
 # Create your views here.
 def home(request):
-    curTime=datetime.datetime.now()
-    html=f"<html><h1>{curTime}</h1></html>"
-    return HttpResponse(html)
-    #return render(request,'home.html')
+    #curTime=datetime.datetime.now()
+    #html=f"<html><h1>{curTime}</h1></html>"
+    #return HttpResponse(html)
+    return render(request,'hi/home.html')

18dc469a28186e197d930caf66e04c7e6b0a73f2
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-2.compute.internal>
2021-06-27 09:04:56 +0000

    bug fix

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-path=''
+path='polls/'
 def index(request):
     return render(request,path+'index.html')
     #html="<html><h2>poll application</h2></html>"
     #return HttpResponse(html)
-def detail(request):
+def detail(request,question_id):
     return render(request,path+'detail.html')
-def results(request):
+def results(request,question_id):
     
     return render(request,path+'results.html')
-def vote(request):
+def vote(request,question_id):
 
     return render(request,path+'vote.html')

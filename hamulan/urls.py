1baad9c29dae2d6b7c376c326981ae5e93cab6c6
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-2.compute.internal>
2021-06-27 08:46:59 +0000

    polls

--- hamulan/urls.py
+++ hamulan/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import hi.views
-import polls.views
+#import polls.views
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',hi.views.home,name='home'),
-    path('polls/',polls.views.polls_func,name='home')
-    ]
+    path('polls/',views.index,name='index'),
+    path('polls/<int:question_id>/', views.detail, name='detail'),
+    path('polls/<int:question_id>/results/', views.results, name='results'),        path('polls/<int:question_id>/vote/', views.vote, name='vote')]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def polls_func(request):
-    html="<html><h2>poll application</h2></html>"
-    return HttpResponse(html)
+path=''
+def index(request):
+    return render(request,path+'index.html')
+    #html="<html><h2>poll application</h2></html>"
+    #return HttpResponse(html)
+def detail(request):
+    return render(request,path+'detail.html')
+def results(request):
+    
+    return render(request,path+'results.html')
+def vote(request):
+
+    return render(request,path+'vote.html')

525a6a039caa61afec086be55c454569674cb994
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-2.compute.internal>
2021-06-27 12:07:49 +0000

    polls applications

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from polls.models import Question
 from django.http import HttpResponse
 # Create your views here.
 path='polls/'
 def index(request):
-    return render(request,path+'index.html')
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+    #return render(request,path+'index.html')
     #html="<html><h2>poll application</h2></html>"
     #return HttpResponse(html)
 def detail(request,question_id):

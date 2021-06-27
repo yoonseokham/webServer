4da7a8dd040710e868cb2d8e1aed2ba5d2fe2186
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-2.compute.internal>
2021-06-27 10:31:19 +0000

    urls.py change

--- polls/urls.py
+++ polls/urls.py
@@ -14,18 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include
 from django.urls import path
 import hi.views
 #import polls.views
 from polls import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',hi.views.home,name='home'),
-    path('polls/',include(('polls.urls','polls'),namespace='polls'))
-    #path('polls/',views.index,name='index'),
-    #path('polls/<int:question_id>/', views.detail, name='detail'),
-    #path('polls/<int:question_id>/results/', views.results, name='results'),      
-    #path('polls/<int:question_id>/vote/', views.vote, name='vote')
+   # path('admin/', admin.site.urls),
+   # path('',hi.views.home,name='home'),
+    path('',views.index,name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
     ]
